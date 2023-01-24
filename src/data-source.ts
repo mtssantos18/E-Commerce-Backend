a4672e323da98172d03d316dc70331342fd1e724
Matheus Santos <eumtssantos@outlook.com>
2023-01-24 12:51:02 -0300

    fix: trying to deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,11 +1,12 @@
-import { DataSource, DataSourceOptions } from "typeorm";
-import path from "path";
 import "dotenv/config";
+import path from "path";
+import "reflect-metadata";
+import { DataSource, DataSourceOptions } from "typeorm";
 
 const dataSourceConfig = (): DataSourceOptions => {
   const entitiesPath: string = path.join(__dirname, "./entities/*.{js,ts}");
   const migrationsPath: string = path.join(__dirname, "./migrations/*.{js,ts}");
-  const nodeEnv: string = process.env.NODE_ENV!;
+  const nodeEnv: string | undefined = process.env.NODE_ENV;
 
   if (nodeEnv === "production") {
     return {
