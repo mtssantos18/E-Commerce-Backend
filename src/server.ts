bead7c0e5ca479dc43d8b96c88a1ea69ef6534e8
Matheus Santos <eumtssantos@outlook.com>
2023-01-24 15:41:32 -0300

    fix: configs to deploy

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import app from "./app";
 import AppDataSource from "./data-source";
 
+const PORT = process.env.PORT || 3333;
+
 (async () => {
   await AppDataSource.initialize().catch((err) => {
     console.log("Error during Data Source initialization", err);
   });
 
-  app.listen(3333, () => {
-    console.log("Server running on port 3333");
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 })();
